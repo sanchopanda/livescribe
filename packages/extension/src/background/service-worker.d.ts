899c5fe98f4c42d1e051c9534e9f0d0ec0cc415d
Aleksander <Ivanovaa1992@gmail.com>
2026-01-03 15:09:30 +0300

    Remove compiled files from src/ and update manifest domains

--- packages/extension/src/background/service-worker.d.ts
+++
@@ -1,7 +0,0 @@
-declare let currentStatus: 'idle' | 'connected' | 'recording' | 'error';
-declare let sessionId: string | null;
-declare let offscreenCreated: boolean;
-declare function ensureOffscreen(): Promise<void>;
-declare function sendToOffscreen(message: object): Promise<any>;
-declare function notifyPopup(message: object): void;
-//# sourceMappingURL=service-worker.d.ts.map
